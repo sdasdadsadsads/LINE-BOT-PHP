64b5897d5e929710296c898bea62a40cae05e665
sdasdadsadsads <30415359+sdasdadsadsads@users.noreply.github.com>
2019-02-07 10:04:26 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,16 +1,11 @@
 <?php
 $access_token = 'jvMwp4K7b3c+pYZfyPyL142BBEigMjXh7wJvgn8qu0MJSWHfJrKKz9/+nFMKaBFf7PSbaxd6SBlKr6gyghJEj55Ujj4gDpQzfxW6KjIKqHKrj0g5Mu/gGOoMCcYWA11QXY2BwjyttlekM4FNxlbv4QdB04t89/1O/w1cDnyilFU=';
 
-
-$url = 'https://api.line.me/v2/oauth/verify';
-
-$headers = array('Authorization: Bearer ' . $access_token);
-
-$ch = curl_init($url);
+$url = 'https://api.line.me/v1/oauth/verify';
+$headers = array('Authorization: Bearer ' . $access_token);$ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
-
 echo $result;
